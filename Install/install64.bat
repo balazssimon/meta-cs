322cf892635fe08666056852463e243a7eadb39e
Balazs Simon <balazssimon@outlook.com>
2015-10-06 12:16:13 +0200

    Registry files removed, since VSIX can now register code generator tools

--- Install/install64.bat
+++ Install/install64.bat
@@ -1,8 +1,5 @@
 set GAC_DIR=c:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools
-set OSLO_DIR=c:\Program Files (x86)\Microsoft Oslo\1.0\bin\
 
 "%GAC_DIR%\gacutil.exe" /i "MetaDslx.Core.dll"
 "%GAC_DIR%\gacutil.exe" /i "MetaDslx.Compiler.dll"
 "%GAC_DIR%\gacutil.exe" /i "MetaDslx.VisualStudio.dll"
-
-reg import Install64.reg
--- Install/uninstall64.bat
+++ Install/uninstall64.bat
@@ -1,7 +1,4 @@
 set GAC_DIR=c:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools
-set OSLO_DIR=c:\Program Files (x86)\Microsoft Oslo\1.0\bin
-
-reg import Uninstall64.reg
 
 "%GAC_DIR%\gacutil.exe" /u "MetaDslx.Core"
 "%GAC_DIR%\gacutil.exe" /u "MetaDslx.Compiler"
