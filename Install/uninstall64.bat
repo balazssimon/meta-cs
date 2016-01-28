c58f49a2d53d79b0466897857a9c8e0ef9311034
Balazs Simon <balazssimon@outlook.com>
2016-01-28 17:25:26 +0100

    v0.2 install batch files

--- Install/uninstall64.bat
+++ Install/uninstall64.bat
@@ -1,5 +1,6 @@
 set GAC_DIR=c:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools
 
+"%GAC_DIR%\gacutil.exe" /u "Antlr4.Runtime"
 "%GAC_DIR%\gacutil.exe" /u "MetaDslx.Core"
 "%GAC_DIR%\gacutil.exe" /u "MetaDslx.Compiler"
 "%GAC_DIR%\gacutil.exe" /u "MetaDslx.VisualStudio"
