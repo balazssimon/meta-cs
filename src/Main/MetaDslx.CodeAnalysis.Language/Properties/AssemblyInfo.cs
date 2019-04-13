ac6584de8c5100fb51f6939f7be06d3f0a47486c
Balazs Simon <balazssimon@gmail.com>
2019-04-13 15:07:06 +0200

    Full CSharp added

--- src/Main/MetaDslx.CodeAnalysis.Language/Properties/AssemblyInfo.cs
+++ src/Main/MetaDslx.CodeAnalysis.Language/Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("MetaDslx.Core")]
+[assembly: InternalsVisibleTo("MetaDslx.CodeAnalysis.CSharp")]
--- src/Main/MetaDslx.CodeAnalysis/Properties/AssemblyInfo.cs
+++ src/Main/MetaDslx.CodeAnalysis/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("MetaDslx.Core")]
 [assembly: InternalsVisibleTo("MetaDslx.CodeAnalysis.Language")]
+[assembly: InternalsVisibleTo("MetaDslx.CodeAnalysis.CSharp")]
