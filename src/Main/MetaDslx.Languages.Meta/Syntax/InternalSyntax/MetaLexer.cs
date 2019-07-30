85d8411e3999369c7c4c1c8c57b38717a6d5fa17
Balazs Simon <balazssimon@gmail.com>
2019-07-30 09:55:35 +0200

    v0.10.91

--- src/Main/MetaDslx.Languages.Meta/Syntax/InternalSyntax/MetaLexer.cs
+++ src/Main/MetaDslx.Languages.Meta/Syntax/InternalSyntax/MetaLexer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// Generated from C:\Users\Balazs\source\repos\meta-cs\src\Main\MetaDslx.Languages.Meta\Syntax\InternalSyntax\MetaLexer.g4 by ANTLR 4.6.6
+// Generated from c:\Users\Balazs\source\repos\meta-cs\src\Main\MetaDslx.Languages.Meta\Syntax\InternalSyntax\MetaLexer.g4 by ANTLR 4.6.6
 
 // Unreachable code detected
 #pragma warning disable 0162
